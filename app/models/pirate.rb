18918150f3566b11f51fc75c6dfd5f70abdd2c6a
Tyler Maxwell <tmax818@mac.com>
2018-03-06 13:22:57 -0800

    initial commit.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+
+  attr_reader :name, :height, :weight
+
+   @@all = []
+
+    def initialize(params)
+      @name = params[:name]
+      @height = params[:height]
+      @weight = params[:weight]
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.clear
+      @@all = []
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+     @@all = []
+
+      def initialize(params)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        @@all << self
+      end
+
+      def self.all
+        @@all
+      end
+
+      def self.clear
+        @@all.clear
+      end
 end
